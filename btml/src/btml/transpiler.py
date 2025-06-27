cfda46b8e95c1b40be7fd246e12576f28d59165c
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-06-27 22:26:47 +0100

    typo

--- btml/src/btml/transpiler.py
+++ btml/src/btml/transpiler.py
@@ -90,7 +90,7 @@ def to_string(
                 )
             case HTMLAttribute():
                 result.append(
-                    f"{indent}<{statement.name}{properties_to_string(statement.properties)}{' /' if statement.close else ''}>"
+                    f"{indent}<{statement.name}{properties_to_string(statement.properties)}{' /' if not statement.close else ''}>"
                     + (
                         (
                             f"{
